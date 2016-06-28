9a1cd677ae14f15b2fa41f4dc0104ee7537aa150
gersosa <sosa.gerardo92@gmail.com>
2016-06-28 00:58:39 -0300

    medelo comentario

--- entrega/app/models/ModelComentario.php
+++ entrega/app/models/ModelComentario.php
@@ -11,14 +11,28 @@
                 trim($str);
 				$trozos = explode ("-", $str);
 				if (count($trozos)==3){
-					$año=$trozos[0];
+					$aï¿½o=$trozos[0];
 					$mes=$trozos[1];
 					$dia=$trozos[2];
-					if(checkdate ($mes,$dia,$año)){
+					if(checkdate ($mes,$dia,$aï¿½o)){
 						return true;
 					}
 				}
 				return false;
 	}
-	
+  public function agregarComentario($comentario, $idPublicacion){
+      $sql = $this->conexion->prepare("INSERT INTO `comentario`( `pregunta`, `fec_preg`, `id_publicacion`)
+      VALUES (:c, NOW(), :idp)");
+      $sql->bindParam(':c', $comentario, PDO::PARAM_STR);
+      $sql->bindParam(':idP', $idPublicacion, PDO::PARAM_INT);
+      $sql->execute();
+  }
+  public function responder($idComentario, $respuesta){
+    $sql = $this->conexion->prepare("UPDATE comentario
+										 SET respuesta = :res, fec_rsp = NOW()
+										 WHERE id_comentario = :id ");
+    $sql->bindParam(':res', $respuesta, PDO::PARAM_STR);
+    $sql->bindParam(':id', $idComentario, PDO::PARAM_INT);
+    $sql->execute(); 
+  }
  }
